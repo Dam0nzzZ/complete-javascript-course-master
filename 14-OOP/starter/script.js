4f32a153d8501333c2166cd5a46af4605465767f
Damon <1260476258@qq.com>
2023-10-16 13:15:06 +0800

    some changes and start for No16

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,70 @@
 'use strict';
+
+const Person = function (firstName, bitrhYear) {
+    this.firstName = firstName;
+    this.bitrhYear = bitrhYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+console.log(jonas instanceof Person);
+
+Person.prototype.calcAge = function () {
+    console.log(2023 - this.bitrhYear);
+};
+
+console.log(Person.prototype);
+jonas.calcAge();
+
+console.log(jonas.__proto__.__proto__);
+
+Person.prototype.height = 180;
+console.log(jonas.height);
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+/*
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed + 'km/h';
+};
+Car.prototype.accelerate = function () {
+  this.speed = parseFloat(this.speed) + 10 + 'km/h';
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed = parseFloat(this.speed) - 5 + 'km/h';
+  console.log(this.speed);
+};
+const bmw = new Car(12, 60.1);
+bmw.accelerate();
+bmw.brake();
+bmw.brake();
+*/
+////////////////////////
+class PersonCL {
+    constructor(firstName, bitrhYear) {
+        this.firstName = firstName;
+        this.bitrhYear = bitrhYear;
+    }
+    calcAge() {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        this.age = year - this.bitrhYear;
+        console.log(year, this.age);
+    }
+}
+
+const John = new PersonCL('John', 2001);
+John.calcAge();
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,5 +2,33 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+const countryFlag = document.querySelector('.country__img');
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+
+    const data = JSON.parse(this.responseText)[0];
+    console.log(data);
+
+    //   countryFlag.src = data.flags.png;
+    const html = `<article class="country">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.continents}</h4>
+        <p class="country__row"><span>👫</span>${data.population} people</p>
+        <p class="country__row"><span>🗣️</span>LANG</p>
+        <p class="country__row"><span>💰</span>CUR</p>
+      </div>
+    </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('usa');
+getCountryData('China');
